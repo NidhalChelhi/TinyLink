6215c0e89250431c0953994794d6dd0f1fe644ec
NidhalChelhi <nidhalelchelhi@gmail.com>
2026-01-15 11:13:39 +0100

    feat: add request tracing with x-request-id header
    - Add tracingMiddleware that generates unique UUID for each request
    - Support for incoming x-request-id header (for distributed tracing)
    - Include requestId in all log entries for correlation
    - Return x-request-id in response headers for client tracking

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,27 @@
+const crypto = require("crypto");
 const { httpRequestDuration } = require("./metrics");
+
+// Generate unique request ID
+function generateRequestId() {
+  return crypto.randomUUID();
+}
+
+// Tracing middleware - adds unique request ID to each request
+function tracingMiddleware(req, res, next) {
+  // Use existing request ID from header or generate new one
+  const requestId = req.headers["x-request-id"] || generateRequestId();
+  
+  // Attach to request object for use in handlers
+  req.requestId = requestId;
+  
+  // Add to response headers for client correlation
+  res.setHeader("x-request-id", requestId);
+  
+  // Store start time for duration tracking
+  req.startTime = Date.now();
+  
+  next();
+}
 
 // Metrics middleware
 function metricsMiddleware(req, res, next) {
@@ -15,5 +38,6 @@ function metricsMiddleware(req, res, next) {
 }
 
 module.exports = {
+  tracingMiddleware,
   metricsMiddleware,
 };
